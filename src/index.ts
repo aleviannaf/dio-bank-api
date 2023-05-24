5b1bc8a5f69e4223e028ae27071775214162c829
aleviannaf <alexandrevianna511@gmail.com>
2023-05-24 13:16:38 -0400

    update of the aplication

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hellho World tudo bem?")
+import express, {
+  Application,
+  Request,
+  Response,
+  json
+} from "express";
+import { UserController } from "./controllers/UserController";
+import { router } from "./routes";
+
+
+
+const userController = new UserController();
+
+const server: Application = express();
+
+server.use(json());
+server.use(router);
+
+server.get("/", (request: Request, response: Response): Response => {
+  return response.status(200).json({ message: "DioBank Api" });
+});
+
+
+
+server.listen(5000, () => console.log(`Server on in http://localhost:5000`));
