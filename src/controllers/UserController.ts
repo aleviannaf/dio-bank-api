1b78fabf1fa140b0084a8cdcea729ea2467af656
aleviannaf <alexandrevianna511@gmail.com>
2023-05-24 13:12:45 -0400

    feat(controllers): implementation of the diobank  controllers

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { UserService } from "../services/UserService";
+
+export class UserController {
+  createUser = (request: Request, response: Response): Response => {
+    const userService = new UserService();
+    const user = request.body;
+
+    if(!user.name){
+        return response.status(400).json({message: 'Bad request: name invalid'})
+    }
+    userService.createUser(user.name, user.email)
+    return response.status(201).json({ message: "Usuário criado" });
+  };
+
+  getAllUsers = (request: Request, response: Response): Response =>{
+    const userService = new UserService();
+    const users = userService.getAllUsers();
+    return response.status(200).json({users})
+  }
+}
