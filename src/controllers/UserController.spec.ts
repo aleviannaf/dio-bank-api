ea075d64c805968448c2a754ee7f1df1141104a4
aleviannaf <alexandrevianna511@gmail.com>
2023-05-29 22:23:44 -0400

    test update

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -1,25 +1,65 @@
 import { UserController } from "./UserController";
-import { UserService } from "../services/UserService";
-import { Request } from "express";
+import { UserService } from '../services/UserService'
+import { Request } from 'express'
 import { makeMockResponse } from "../__mocks__/makeMockResponse.mock";
 
-describe("UseController", () => {
-  const mockUserService: Partial<UserService> = {
-    createUser: jest.fn(),
-  };
 
-  const userController = new UserController(mockUserService as UserService);
 
-  it("Deve adicionar um novo usuário", () => {
+describe('UserController', () => {
+    const mockUserService: Partial<UserService> = {
+        createUser: jest.fn(),
+        getAllUsers: jest.fn(),
+    }
     const mockRequest = {
-      body: {
-        name: "Nath",
-        email: "nath@test.com",
-      },
-    } as Request;
-    const mockResponse = makeMockResponse();
-    userController.createUser(mockRequest, mockResponse);
-    expect(mockResponse.state.status).toBe(201);
-    expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado'})
-  });
-});
+        body: {
+            name: 'Nath',
+            email: 'nath@test.com'
+        }
+    } as Request
+    const mockResponse = makeMockResponse()
+    const userController = new UserController(mockUserService as UserService);
+
+    it('Deve adicionar um novo usuário', () => {
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(201)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado' })
+    })
+
+    it('Deve retornar: Bad request! Por causa do email ausente', ()=>{
+        const mockRequest = {
+            body: {
+                name: 'nath'
+            }
+        } as Request
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(400)
+    })
+
+    it('Deve retornar: Bad request! Por causa do nome ausente', ()=>{
+        const mockRequest = {
+            body: {
+                email: 'nath@test.com'
+            }
+        } as Request
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(400)
+    })
+
+    it('Deve retornar a lista de usuários', ()=>{
+        userController.getAllUsers(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(200);
+    })
+
+    it('Deve retornar a mensagem de usuário deletado', ()=>{
+        const mockRequest = {
+            body: {
+                name: 'Nath',
+                email: ''
+            }
+        } as Request
+
+        userController.deleteUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(200)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Usuário deletado'})
+    })
+})
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,24 +1,34 @@
-import { Request, Response } from "express";
-import { UserService } from "../services/UserService";
+import { Request, Response } from 'express'
+import { UserService } from '../services/UserService'
 
 export class UserController {
-  userService : UserService;
+    userService: UserService
 
-  constructor(userService = new UserService()){
-    this.userService = userService
-  }
-  createUser = (request: Request, response: Response): Response => {
-    const user = request.body;
-
-    if(!user.name){
-        return response.status(400).json({message: 'Bad request: name invalid'})
+    constructor(
+        userService = new UserService()
+    ){
+        this.userService = userService
     }
-    this.userService.createUser(user.name, user.email)
-    return response.status(201).json({ message: "Usuário criado" });
-  };
 
-  getAllUsers = (request: Request, response: Response): Response =>{
-    const users = this.userService.getAllUsers();
-    return response.status(200).json({users})
-  }
+    createUser = (request: Request, response: Response): Response => {
+        const user = request.body
+
+        if(!user.name || !user.email){
+            return response.status(400).json({ message: 'Bad request! Campo obrigatório ausente'})
+        }
+
+        this.userService.createUser(user.name, user.email)
+        return response.status(201).json({ message: 'Usuário criado'})
+    }
+
+    getAllUsers = (request: Request, response: Response) => {
+        const users = this.userService.getAllUsers()
+        return response.status(200).json( users )
+    }
+    
+    deleteUser =(request: Request, response: Response) =>{
+        const user = request.body
+        console.log('Deletando Usuário...', user)
+        return response.status(200).json({ message: 'Usuário deletado'})
+    }
 }
